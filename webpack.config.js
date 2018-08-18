11857576c076f225a7197fe97f4d132d13263a0d
mraiguo <810158465@qq.com>
2018-08-18 23:43:00 +0800

    使用ExtractTextPlugin分离css文件（ExtractTextPlugin还没有webpack4版本，使用npm install extract-text-webpack-plugin@next解决）

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const path = require('path'); // 引入node的path模块
+const path = require('path') // 引入node的path模块
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 const config = {
     entry: { // 设置打包的入口文件是相对当前路径的app.js文件
@@ -13,7 +14,11 @@ const config = {
         rules:[
             {
                 test: /\.(scss|css)$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    //resolve-url-loader may be chained before sass-loader if necessary
+                    use: ['css-loader', 'sass-loader']
+                })
             }
         ],
     },
@@ -25,8 +30,9 @@ const config = {
             hash: true,       // true | false。如果是true，会给所有包含的script和css添加一个唯一的webpack编译hash值。这对于缓存清除非常有用。
             inject: true,     // | 'head' | 'body' | false  ,注入所有的资源到特定的 template 或者 templateContent 中，如果设置为 true 或者 body，所有的 javascript 资源将被放置到 body 元素的底部，'head' 将放置到 head 元素中。
             chunks: ["main"]   // 使用chunks 需要指定entry 入口文件中的哪一个模块
-        })
+        }),
+        new ExtractTextPlugin('style.css')
     ]
 };
 
-module.exports = config;
+module.exports = config
